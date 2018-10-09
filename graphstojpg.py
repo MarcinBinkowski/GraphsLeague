879add34b3ac0c5c6ca80c6374a142890d347b04
marcinbinkowski <marcin.binkowski@onet.eu>
2018-10-10 01:05:01 +0200

    working masteries graph

--- graphstojpg.py
+++ graphstojpg.py
@@ -15,15 +15,11 @@ class GraphData:
         self.best_three_ids = [x[0] for x in self.masteries[0]]
         self.best_three_names = self.get_champions_names(self.best_three_ids)
 
-        self.worst_score = [self.masteries[1][0][1]]
-        self.worst_id = [self.masteries[1][0][0]]
-        self.worst_name = [self.get_champion_name(str(self.worst_id[0]))]
-
         self.all_champions_score = modifydata.get_all_champs_score(summoner)
-        self.all_champions_score_modified = [self.all_champions_score - sum(self.best_three_scores) - self.worst_score[0]]
+        self.all_champions_score_modified = [self.all_champions_score - sum(self.best_three_scores)]
 
-        self.donut_chart_data_scores = self.best_three_scores + self.all_champions_score_modified + self.worst_score
-        self.donut_chart_data_names = self.best_three_names + ["Rest"] + self.worst_name
+        self.donut_chart_data_scores = self.all_champions_score_modified + self.best_three_scores[::-1]
+        self.donut_chart_data_names = ["Rest"] + self.best_three_names[::-1]
         print(self.donut_chart_data_scores)
         print(self.donut_chart_data_names)
 
@@ -45,18 +41,17 @@ class GraphData:
         return temp
 
 
+def champions_distribiution_graph(graph_data):
+    names = graph_data.donut_chart_data_names
+    scores = graph_data.donut_chart_data_scores
 
-
-
-
-if __name__ == "__main__":
-    gaph_data = GraphData(NewUser("binq661", "eune"))
-    names = gaph_data.donut_chart_data_names
-    scores = gaph_data.donut_chart_data_scores
-
-    plt.pie(scores)
+    plt.pie(scores, labels=names, startangle=90, colors=['#ce0404', 'green', 'blue', 'purple'])
     my_circle = plt.Circle((0, 0), 0.7, color='white')
     p = plt.gcf()
     p.gca().add_artist(my_circle)
+    plt.savefig('{}_mastery_distribution.png'.format(gaph_data.summoner.summoner), transparent=False)
 
-    plt.show()
+if __name__ == "__main__":
+    gaph_data = GraphData(NewUser("binq661", "eune"))
+    champions_distribiution_graph(gaph_data)
+
